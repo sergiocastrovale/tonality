7137d5168ef1464c03b86f557f1bf17c98828ea9
Raquel Freitas <raquel.go.freitas@gmail.com>
2019-09-25 12:01:09 +0100

    feat(about): added about page

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container mx-auto px-4 mt-8">
+    <h3 class="text-2xl font-semibold mb-2">About</h3>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis accusantium maiores commodi sit laborum magni velit? Ab ipsum exercitationem sit neque ratione qui, quod necessitatibus aliquid adipisci iste, ut eaque.
+    </div>
+  </div>
+</template>
